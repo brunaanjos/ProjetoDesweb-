d8fbbe4d3450f27293c74afa4315d0c95c5cfd53
brunaanjos <brunaanjosdaveiga@gmail.com>
2023-03-31 11:51:46 -0300

    310323

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+const nome = ''
+const Email = ''
+</script>
+
+<template>
+  <h1>Perfil Do Usuario</h1>
+  <div>
+  <input type="text" v-model="nome" 
+  placeholder="nome" required/>
+  </div>
+  
+  <div>
+    <input type="email" v-model="Email" 
+    placeholder="Email" required/>
+  </div>
+  <div>
+<input type="Senha" v-model="Senha" placeholder="Senha" required>
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
